619998ee5880955bf59f13970017159bc1d789ec
Namrahawan <farzeenarshad1@gmail.com>
2025-01-28 16:05:26 +0500

    added two folders

---
+++ Quarter_Details/Classworks/Class Work.py
@@ -0,0 +1,71 @@
+# Comparison operators
+# num1 = 4
+# num2 = 4
+# ==, >, <, <=, >=, !=
+# print( num1 == num2 )
+# print(num1 <= num2) # True
+# print(num1 >= num2) # True
+# print(num1 != num2)
+
+# country1 = "pakistan"
+# country2 = "PAKISTAN"
+
+#        pakistan      ==    pakistan
+# print(country1.lower() == country2.lower())
+
+# print( country1.upper() ) # PAKISTAN
+# print( country1.title() ) # Pakistan Zindabad
+# print( country1.capitalize() ) # Pakistan zindabad
+
+
+# age = 11
+# #       True
+# if (  age >= 18 ):
+#     print("You're Allowed")
+#     print("Enjoy!!!!!")
+# else:
+#     print("You're not allowed")
+
+# percentage = 102
+
+# #          true      and        false
+# if (percentage >= 90 and percentage <= 100):
+#     print("Your grade is A+")
+
+# elif (percentage >= 80 and percentage < 90):
+#     print("Your grade is A")
+
+# elif (percentage >= 70 and percentage < 80):
+#     print("your grade is B")
+
+# else :
+#     print("you are failed")
+
+# and Operator
+# has_good_job = True
+# can_cook = True
+# has_civic = False
+# age = 21
+# #       True     and   True  and  False
+# if (has_good_job and can_cook and has_civic ):
+#     print("Rhista Pakka")
+# else:
+#     print("Rejected")
+
+# Or Operator
+# is_ameer = False
+# is_sir_hamzah_std = True
+
+# if is_ameer or is_sir_hamzah_std:
+#     print('Rishta Pakka')
+
+# else:
+#     print('Rejected')
+
+
+# Boolean
+# Comparison Operators(==, !=, >, <, <=, >=)
+# lower(), upper(), title(), capitalize()
+# if, elif, else
+# Logical operators (and, or, not)
+
